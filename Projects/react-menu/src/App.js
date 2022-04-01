e92e4258c48e8170bb5bd6af47f690b5185dbf50
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-01 12:26:19 +0530

    Update app.js and app.css

--- Projects/react-menu/src/App.js
+++ Projects/react-menu/src/App.js
@@ -1,16 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 import MenuList from "./components/Menus/MenuList";
 import { menuItems } from "./menus";
 
 function App() {
-	const [category, setcategory] = useState([]);
+	const [category, setCategory] = useState([]);
 	const [menus, setMenus] = useState(menuItems);
+	useEffect(() => {
+		const newCategory = [];
+		menuItems.map((ele) => {
+			const { category } = ele;
+			if (newCategory.length) {
+				if (newCategory.includes(category) === false) {
+					newCategory.push(category);
+				}
+			} else {
+				newCategory.push(category);
+			}
+
+			return newCategory;
+		});
+		setCategory(newCategory);
+	}, []);
+
+	const onClickHandler = (type) => {
+		if (type) {
+			const newMenuList = menuItems.filter((menu) => menu.category === type);
+			return setMenus(newMenuList);
+		} else setMenus(menuItems);
+	};
+
 	return (
 		<div className="container">
 			<header>Our Menu</header>
 			<div className="underline"></div>
-			<nav>Food Category</nav>
+			<nav className="navbar">
+				<button className="btn" onClick={() => onClickHandler()}>
+					All
+				</button>
+				{category.map((cat, ind) => (
+					<div key={ind}>
+						{<Button type={cat} onClickHandler={onClickHandler} />}
+					</div>
+				))}
+			</nav>
 			<main>
 				{menus.map((menu) => (
 					<MenuList key={menu.id} {...menu} />
