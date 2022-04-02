dc395b3232b0bc183fc53fa0f4bd65035f73e521
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-03 00:32:19 +0530

    Created new react grossary bud project

--- Projects/react-color-generator/src/components/MultiColorList/MultiColorList.jsx
+++ Projects/react-color-generator/src/components/MultiColorList/MultiColorList.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import SingleColor from "../SingleColor.jsx/SingleColor";
-import './MultiColorStyles.css'
-
+import "./MultiColorStyles.css";
 
 const MultiColorList = ({ hex, weight, textColor }) => {
+	const styles = {
+		backgroundColor: `#${hex}`,
+		color: textColor,
+		border: `2px solid #${hex}`,
+	};
+
 	return (
-		<div
-			className="color-card"
-			style={{
-				backgroundColor: `#${hex}`,
-				color: textColor,
-				border: `2px solid #${hex}`,
-				
-			}}
-		>
+		<div className="color-card" style={styles}>
 			<SingleColor hex={hex} weight={weight} />
 		</div>
 	);
