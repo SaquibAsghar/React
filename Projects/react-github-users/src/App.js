21238522a57abc08449ed812eaaab512adf60b43
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-10 23:02:21 +0530

    added css

--- Projects/react-github-users/src/App.js
+++ Projects/react-github-users/src/App.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleUser from "./components/SingleUser/SingleUser";
 
 function App() {
+  const [user, setUser] = useState([])
+
+  const getSingleUser = async()=>{
+    const response = await fetch("https://api.github.com/users/SaquibAsghar")
+    const data = await response.json()
+    console.table(data)
+    setUser([data])
+
+  } 
+
+  useEffect(()=>{
+    getSingleUser()
+  }, [])
 	return (
 		<div className="App">
 			<h1>Github Users</h1>
-      <SingleUser />
+      {
+        // console.log(user)
+      }
+      <SingleUser user = {user} />
 		</div>
 	);
 }
--- Projects/react-github-users/src/components/SingleUser/SingleUser.jsx
+++ Projects/react-github-users/src/components/SingleUser/SingleUser.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./SingleUserStyle.css";
 
-const SingleUser = () => {
-  return (
-    <div>SingleUser</div>
-  )
-}
+const SingleUser = ({ user }) => {
+	const githubUser = { ...user };
+	console.log(githubUser);
+	return (
+		<section className="card-container">
+			<div className="card">
+				<div className="card-content">
+					<div className="card-img"></div>
+					<div className="card-info">
+						<h4>Name : {user[0].name}</h4>
+						<p>UserName : {user[0].login}</p>
+						<p>Repository : {user[0].public_repos}</p>
+						<p>Followers : {user[0].followers}</p>
+						<div className="card-cta">
+							<a
+								className="card-cta--link"
+								href={user[0].html_url}
+								target="#blank"
+							>
+								View Profile
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default SingleUser
+export default SingleUser;
