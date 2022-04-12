cf8891c6e2f02c0c535301b527d0d6d38e910af4
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-12 17:11:00 +0530

    Added useSelector
    Added useSelecter hook from react-redux which take callback fnc with a argument state and return the latest state object from the store

--- Projects/react-redux/src/App.js
+++ Projects/react-redux/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-     <h3>React Router</h3>
-    </div>
-  );
+	const { count } = useSelector((storeState) => storeState);
+	// useSelector is hook of react-redux which provide state from store to the component.
+	// It take a callback function with arg state and return the state object
+
+	return (
+		<div className="App">
+			<h3>React Router</h3>
+			<h4>Counter App</h4>
+			<h5>Counter : {count}</h5>
+		</div>
+	);
 }
 
 export default App;
