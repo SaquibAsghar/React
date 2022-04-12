1c2f134b6c72d5adab0ca0ce023347bca4193e26
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-12 12:31:11 +0530

    Added single product page

--- Projects/react-router/src/pages/SingleProduct.jsx
+++ Projects/react-router/src/pages/SingleProduct.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import {useParams, Link} from 'react-router-dom'
 
 const SingleProduct = () => {
+	const {productID} = useParams()
 	return (
 		<section className="section">
-			<h2>Welcome to Single product page</h2>
+			<h2>Product page {productID}</h2>
+			<Link to="/products">Back To Products</Link>
 		</section>
 	);
 };
