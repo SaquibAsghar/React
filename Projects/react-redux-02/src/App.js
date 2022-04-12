eb08ed1e3e02f8279311e6f300d81dd8c74ad904
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-12 17:54:43 +0530

    created new react-redux project

---
+++ Projects/react-redux-02/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>React Redux 02 </h2>
+     <h4>Redux toolkit</h4>
+    </div>
+  );
+}
+
+export default App;
