481189a4e180e34a2353686ddacc201113c28b58
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-12 15:28:59 +0530

    added Protected route in dashboard

--- Projects/react-router/src/pages/ProtectedRoute.jsx
+++ Projects/react-router/src/pages/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
-}
+const ProtectedRoute = ({ children, user }) => {
+	if (!user) return <Navigate to="/" />;
+	return children;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
