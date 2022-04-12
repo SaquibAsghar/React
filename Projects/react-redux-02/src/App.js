7e5a49a95b4f4c08d6099a9b99d2056cf9997a01
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-12 18:57:58 +0530

    Added action from store/index.js

--- Projects/react-redux-02/src/App.js
+++ Projects/react-redux-02/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import { actions } from './store';
+
 
 function App() {
+  const {count} = useSelector(state => state )
+  const dispatch = useDispatch()
+
+  const onAdd = () =>{
+    dispatch(actions.increment(4))
+    
+  }
+  const onDec = () =>{
+    dispatch(actions.decrement(-1))
+
+  }
+  const onAddTo = () =>{
+    dispatch(actions.addToIt(10))
+  }
+
+
   return (
     <div className="App">
-     <h2>React Redux 02 </h2>
      <h4>Redux toolkit</h4>
+     <h4>Counter App</h4>
+     <h5>Count : {count}</h5>
+     <button onClick={onAdd}>Increment</button>
+     <button onClick={onDec}>Decrement</button>
+     <button onClick={onAddTo}>AddTo10</button>
     </div>
   );
 }
