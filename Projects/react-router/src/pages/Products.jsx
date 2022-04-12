8486a4b04d591290ae72c249921abfdcc4c36c3b
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2022-04-12 12:59:52 +0530

    add product data to page

--- Projects/react-router/src/pages/Products.jsx
+++ Projects/react-router/src/pages/Products.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import products from "../data";
 const Products = () => {
 	return (
 		<section className="section product">
 			<h2>Products page</h2>
-			
+
+			{products.map((product) => {
+				const { id, name } = product;
+				return (
+					<article key={id}>
+						<h5>{name}</h5>
+						<Link to={`/products/${id}`}>More info</Link>
+					</article>
+				);
+			})}
 		</section>
 	);
 };
--- Projects/react-router/src/pages/SingleProduct.jsx
+++ Projects/react-router/src/pages/SingleProduct.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import {useParams, Link} from 'react-router-dom'
+import { useParams, Link } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
-	const {productID} = useParams()
+	const { productID } = useParams();
+	const product = products.find((prod) => prod.id === productID);
+	const { image, name } = product;
 	return (
-		<section className="section">
-			<h2>Product page {productID}</h2>
+		<article className="section" style={{ margin: "20px" }}>
+			<img src={image} alt={name} style={{ width: "400px" }} />
+			<h3>{name}</h3>
 			<Link to="/products">Back To Products</Link>
-		</section>
+		</article>
 	);
 };
 
